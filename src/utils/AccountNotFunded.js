8d564aead85296c76ab7e228acd832ef22c36c89
rafucisneros <rafucisneros@gmail.com>
2019-10-24 19:49:46 -0400

    texto para la pantalla account not funded

--- src/utils/AccountNotFunded.js
+++ src/utils/AccountNotFunded.js
@@ -1,12 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { View, Text } from 'react-native'
+import styles from './Styles'
 
 class AccountNotFunded extends Component{
   render(){
     return (
-      <View>
-        <Text>Account not funded</Text>
-      </View>
+      <Fragment>
+        <View style={{display: "flex", flexDirection: "row", justifyContent: "center"}}>
+          <Text style={[styles.sectionTitle]}>Account not funded</Text>
+        </View>
+        <View style={{display: "flex", flexDirection: "row"}}>
+          <Text>This account is not funded yet. You need to receive at least 1 XLM in a "Create Account" operation in order to fund your account.</Text>
+        </View>
+      </Fragment>
     )
   }
 }
